e7a798a4652d8d5689cc8a7e368485e55a58b6ab
Kasper Olsen <kols@netlight.com>
2022-09-06 10:26:14 +0200

    added ping endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/svopper/kalles_weather_dashboard_v2/pkg/common/router"
 	"github.com/svopper/kalles_weather_dashboard_v2/pkg/metObs"
 	"github.com/svopper/kalles_weather_dashboard_v2/pkg/oceanObs"
@@ -17,7 +18,11 @@ func main() {
 		log.Println("PORT environment variable not set. Defaulting to 8080")
 		port = "8080"
 	}
-
+	router.GET("", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
 	metObs.RegisterRoutes(router)
 	oceanObs.RegisterRoutes(router)
 	router.Run("localhost:8080")
